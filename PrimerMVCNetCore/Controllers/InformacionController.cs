c8b1216fc33745c8efbd5a17ef2ebc929a0d79d6
AlexRR03 <alexciempo03@gmail.com>
2025-01-28 09:41:01 +0100

    Add project files.

---
+++ PrimerMVCNetCore/Controllers/InformacionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerMVCNetCore.Models;
+
+namespace PrimerMVCNetCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(string variable1, string variable2)
+        {
+
+            return View();
+        }
+
+        public IActionResult ControladorVista()
+        {
+            //Enviar informacion simple a la vista
+            ViewData["Nombre"] = "Alejandro";
+            ViewData["Edad"] = 22;
+            ViewBag.DiaSemana = "Lunes";
+
+            return View();
+        }
+        public IActionResult ControladorVistaModel()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Alejandro";
+            persona.Email = "alejandro@email.com";
+            persona.Edad = 22;
+            return View(persona);
+        }
+        public IActionResult VistaControllerGet(string saludo, int year) 
+        {
+            ViewData["DATA"] = "Hola " + saludo + ", año:" +year;
+            return View();
+        }
+    }
+}
